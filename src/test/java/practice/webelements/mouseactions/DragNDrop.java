4b46472a23e6f55e95547e04fb06896069973552
Ganesh K <ganeshkannan627@gmail.com>
2023-08-30 10:13:10 +0530

    mouse actions

--- src/test/java/practice/webelements/mouseactions/DragNDrop.java
+++ src/test/java/practice/webelements/mouseactions/DragNDrop.java
@@ -6,21 +6,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
+import java.time.Duration;
+
 public class DragNDrop {
     public static void main(String[] args) {
         WebDriver driver= new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("http://demo.guru99.com/test/drag_drop.html");
 
         //Element which needs to drag.
-        WebElement from=driver.findElement(By.xpath("//*[@id='credit2']/a"));
+        WebElement from = driver.findElement(By.id("fourth"));
 
         //Element on which need to drop.
-        WebElement to=driver.findElement(By.xpath("//*[@id='bank']/li"));
+        WebElement to = driver.findElement(By.xpath("//ol[@id='amt7']"));
 
         //Using Action class for drag and drop.
-        Actions act=new Actions(driver);
+        Actions actions=new Actions(driver);
 
         //Dragged and dropped.
-        act.dragAndDrop(from, to).build().perform();
+        actions.dragAndDrop(from, to).build().perform();
     }
 }
